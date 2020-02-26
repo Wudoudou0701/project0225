c996228eb7f28a16fe38dd648161fcff9388e4aa
wyf <272504673@qq.com>
2020-02-26 16:35:44 +0800

    add store mybit files

---
+++ jcart-store-back/src/main/java/io/wyf/jcartstoreback/dao/OrderDetailMapper.java
@@ -0,0 +1,19 @@
+package io.wyf.jcartstoreback.dao;
+
+import io.wyf.jcartstoreback.po.OrderDetail;
+
+public interface OrderDetailMapper {
+    int deleteByPrimaryKey(Long orderId);
+
+    int insert(OrderDetail record);
+
+    int insertSelective(OrderDetail record);
+
+    OrderDetail selectByPrimaryKey(Long orderId);
+
+    int updateByPrimaryKeySelective(OrderDetail record);
+
+    int updateByPrimaryKeyWithBLOBs(OrderDetail record);
+
+    int updateByPrimaryKey(OrderDetail record);
+}
---
+++ jcart-store-back/src/main/java/io/wyf/jcartstoreback/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package io.wyf.jcartstoreback.dao;
+
+import io.wyf.jcartstoreback.po.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer productId);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer productId);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
