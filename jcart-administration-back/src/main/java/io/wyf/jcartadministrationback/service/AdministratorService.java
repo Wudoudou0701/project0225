f9ab684ef07403fe23bc5fd54eead2fabc962fde
wyf <272504673@qq.com>
2020-03-02 18:33:37 +0800

    add  admin  service

--- jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/AdministratorService.java
+++ jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/AdministratorService.java
@@ -1,6 +1,9 @@
 package io.wyf.jcartadministrationback.service;
 
+import com.github.pagehelper.Page;
 import io.wyf.jcartadministrationback.po.Administrator;
+
+import java.util.List;
 
 public interface AdministratorService {
 
@@ -8,5 +11,13 @@ public interface AdministratorService {
 
         Administrator getByUsername(String username);
 
+        Integer create(Administrator administrator);
+
         void update(Administrator administrator);
+
+        void delete(Integer administratorId);
+
+        void batchDelete(List<Integer> administratorIds);
+
+        Page<Administrator> getList(Integer pageNum);
 }
