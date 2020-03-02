82784d3e39defe260153781b80102d66f25c54fa
wyf <272504673@qq.com>
2020-03-02 18:41:48 +0800

    add  cutomer   mapper

--- jcart-store-back/src/main/java/io/wyf/jcartstoreback/dao/CustomerMapper.java
+++ jcart-store-back/src/main/java/io/wyf/jcartstoreback/dao/CustomerMapper.java
@@ -18,4 +18,6 @@ public interface CustomerMapper {
 
     //    custom
     Customer selectByUsername(@Param("username") String username);
+
+    Customer selectByEmail(@Param("email") String email);
 }
