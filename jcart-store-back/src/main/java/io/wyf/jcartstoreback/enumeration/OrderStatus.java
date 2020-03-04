4cdb45da4536522dfbcb72951a406b6841983cee
wyf <272504673@qq.com>
2020-03-04 15:43:32 +0800

    add   order   enmeration

---
+++ jcart-store-back/src/main/java/io/wyf/jcartstoreback/enumeration/OrderStatus.java
@@ -0,0 +1,17 @@
+package io.wyf.jcartstoreback.enumeration;
+
+public class OrderStatus {
+    ToProcess,
+    Processing,
+    ToShip,
+    Shipped,
+    ToReceive,
+    Received,
+    ToPay,
+    Paid,
+    Cancel,
+    Denied,
+    Completed,
+    ToComment,
+    Commented
+}
