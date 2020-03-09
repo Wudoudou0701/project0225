4c3aa3bfbce51d7d4cef164062413679f9a50140
wyf <272504673@qq.com>
2020-03-09 19:25:57 +0800

    add  return r service

--- jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/ReturnService.java
+++ jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/ReturnService.java
@@ -1,11 +1,12 @@
 package io.wyf.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.wyf.jcartadministrationback.dto.in.ReturnSearchInDTO;
 import io.wyf.jcartadministrationback.po.Return;
 
 public interface ReturnService {
 
-    Page<Return> search(Integer pageNum);
+    Page<Return> search(ReturnSearchInDTO returnSearchInDTO, Integer pageNum);
 
     Return getById(Integer returnId);
 
