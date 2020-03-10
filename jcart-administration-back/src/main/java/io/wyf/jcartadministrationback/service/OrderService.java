59f8d8888ce29a2297f980d9a90145562374bc41
wyf <272504673@qq.com>
2020-03-10 19:38:38 +0800

    add   administration  search  controller  api

--- jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/OrderService.java
+++ jcart-administration-back/src/main/java/io/wyf/jcartadministrationback/service/OrderService.java
@@ -1,13 +1,14 @@
 package io.wyf.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.wyf.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.wyf.jcartadministrationback.dto.out.OrderListOutDTO;
 import io.wyf.jcartadministrationback.dto.out.OrderShowOutDTO;
 import io.wyf.jcartadministrationback.po.Order;
 
 public interface OrderService {
 
-    Page<OrderListOutDTO> search(Integer pageNum);
+    Page<OrderListOutDTO> search(OrderSearchInDTO orderSearchInDTO, Integer pageNum);
 
     OrderShowOutDTO getById(Long orderId);
 
