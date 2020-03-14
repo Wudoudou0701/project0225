9463f523aa26d71698bf80529e8491c9fd236c84
wyf <272504673@qq.com>
2020-03-14 21:17:42 +0800

    fix return searcg route page js

--- jcart-administration-front/route-page/return-search-route-page.js
+++ jcart-administration-front/route-page/return-search-route-page.js
@@ -1,25 +1,31 @@
 const ReturnSearchRoutePage = {
     template: `
     <div id="app">
+    
         <el-input v-model="returnId" placeholder="请输入退货Id"></el-input>
         <el-input v-model="orderId" placeholder="请输入订单Id"></el-input>
         <el-input v-model="customerName" placeholder="请输入客户姓名"></el-input>
         <el-input v-model="productCode" placeholder="请输入商品代号"></el-input>
         <el-input v-model="productName" placeholder="请输入商品名称"></el-input>
         <br>
+        
         <el-select v-model="selectedStatus" placeholder="请选择状态">
             <el-option v-for="item in statuses" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
         </el-select>
         <br>
+        
         <el-date-picker v-model="startTime" type="date" placeholder="选择开始日期">
         </el-date-picker>
         <br>
+        
         <el-date-picker v-model="endTime" type="date" placeholder="选择结束日期">
         </el-date-picker>
         <br>
+        
         <el-button type="primary" @click="handleSearchClick">搜索</el-button>
         <el-button type="primary" @click="handleClearClick">清空条件</el-button>
+        
         <el-table :data="pageInfo.list" style="width: 100%">
             <el-table-column prop="returnId" label="退货Id">
             </el-table-column>
@@ -46,7 +52,13 @@ const ReturnSearchRoutePage = {
                     {{(new Date(scope.row.updateTimestamp)).toLocaleString()}}
                 </template>
             </el-table-column>
+             <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="primary" @click="handleEditClick(scope.$index, scope.row)">编辑</el-button>
+                </template>
+            </el-table-column>
         </el-table>
+        
         <el-pagination layout="prev, pager, next" :total="pageInfo.total" @current-change="handlePageChange">
         </el-pagination>
     </div>
@@ -92,6 +104,9 @@ const ReturnSearchRoutePage = {
             this.selectedStatus = '';
             this.startTime = '';
             this.endTime = '';
+        },
+        handleEditClick(index, row) {
+            this.$router.push('/return/edit/' + row.returnId + '/show');
         },
         handlePageChange(val) {
             console.log('page changed', val);
